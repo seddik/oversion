681c011097202f4277c527a2130870df1e57165e
LARABA MOHAMMED SEDDIK <mslaraba@gmail.com>
2021-08-03 22:49:40 +0100

    Initial commit

---
+++ VersionConfiguration.cs
@@ -0,0 +1,135 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task = System.Threading.Tasks.Task;
+
+namespace oVersion
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class VersionConfiguration
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("834ae753-3ec2-4749-999f-57030dcd3dec");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VersionConfiguration"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private VersionConfiguration(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static VersionConfiguration Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in VersionConfiguration's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new VersionConfiguration(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = "II";
+            string title = "VersionConfiguration";
+
+            var dte2 = (DTE2)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(SDTE));
+
+            title = dte2.Solution.FullName;
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                VsShellUtilities.ShowMessageBox(
+               this.package,
+               "No solution found",
+               "ERROR",
+               OLEMSGICON.OLEMSGICON_CRITICAL,
+               OLEMSGBUTTON.OLEMSGBUTTON_OK,
+               OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                return;
+            }
+
+            if (dte2.Solution.Projects.Count == 0)
+            {
+                VsShellUtilities.ShowMessageBox(
+               this.package,
+               "No project found",
+               "ERROR",
+               OLEMSGICON.OLEMSGICON_CRITICAL,
+               OLEMSGBUTTON.OLEMSGBUTTON_OK,
+               OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                return;
+            }
+
+            var vcForm = new wVersion(dte2.Solution.Projects);
+            vcForm.ShowModal();
+
+
+
+
+
+        }
+    }
+}
---
+++ wVersion.xaml.cs
@@ -0,0 +1,136 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace oVersion
+{
+    /// <summary>
+    /// Interaction logic for wVersion.xaml
+    /// </summary>
+    public partial class wVersion : DialogWindow
+    {
+
+        List<Project> Projects { get; set; } = new List<Project>();
+
+        public wVersion()
+        {
+
+        }
+        public wVersion(EnvDTE.Projects projs)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            InitializeComponent();
+            b_save.Click += B_save_Click;
+            b_cancel.Click += B_cancel_Click;
+
+            foreach (Project item in projs)
+            {
+                this.Projects.Add(item);
+            }
+
+            lst.ItemsSource = this.Projects;
+            lst.DisplayMemberPath = "Name";
+            lst.SelectAll();
+
+            lst.SelectionChanged += Lst_SelectionChanged;
+
+            var props = Projects[0].Properties;
+
+            var prop = GetProp(props, "Version") ?? GetProp(props, "AssemblyVersion");
+            if (prop == null)
+                return;
+
+            var version = prop.Value as Version;
+            if (version == null)
+                version = GetVersion(prop.Value.ToString());
+
+            if (version == null)
+                return;
+
+            t_major.Text = version.Major.ToString();
+            t_minor.Text = version.Minor.ToString();
+            t_build.Text = version.Build.ToString();
+            t_rev.Text = version.Revision.ToString();
+
+            Lst_SelectionChanged(null, null);
+        }
+
+        Version GetVersion(string version)
+        {
+            try
+            {
+                return new Version(version);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        Property GetProp(Properties props, string path)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                return props.Item(path);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void B_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Lst_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            h_count.Text = "Selected projects : " + lst.SelectedItems.Count;
+
+
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        private void B_save_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            foreach (Project item in lst.SelectedItems)
+            {
+                var prop = GetProp(item.Properties, "Version") ?? GetProp(item.Properties, "AssemblyVersion");
+                if (prop == null)
+                    continue;
+
+                prop.Value = new Version(int.Parse(t_major.Text), int.Parse(t_minor.Text), int.Parse(t_build.Text), int.Parse(t_rev.Text));
+                item.Save();
+            }
+
+            Close();
+        }
+    }
+}
